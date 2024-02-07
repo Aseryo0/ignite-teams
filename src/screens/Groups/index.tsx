3574ecfe5155fbb46bbcb0eba4c01e3dc47a4c8c
Aseryo0 <andrefelipeil@hotmail.com>
2024-02-07 12:04:45 -0300

    base do projeto pronta

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export const Groups = () => {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+};
